6103240b089a76d4f579d4e745bbc856f740e8f4
Alan Zhu <xiaozhizhu1994@gmail.com>
2026-01-20 10:54:51 -0800

    feat: Add Google Analytics (GA4) measurement ID

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import 'katex/dist/katex.min.css'
 import '../styles/globals.css'
-
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Script
+                src="https://www.googletagmanager.com/gtag/js?id=G-L37FLSK1TJ"
+                strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-L37FLSK1TJ');
+        `}
+            </Script>
+            <Component {...pageProps} />
+        </>
+    )
 }
